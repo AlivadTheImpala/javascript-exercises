1ea68b30ea15d0a701b5bed22a314b8d4527932e
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-12-28 19:04:41 -0800

    exercise 02 returns ERROR with negative number: PASSED

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function (string, repeatNum) {
   let repeatedString = "";
+  if (repeatNum < 0) {
+    return "ERROR";
+  }
+
   for (let i = 0; i < repeatNum; i++) {
     repeatedString += string;
   }
