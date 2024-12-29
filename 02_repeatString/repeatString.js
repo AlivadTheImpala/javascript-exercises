7b3bed94c7a8e163addc8b2229fa4e99ef941acc
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-12-28 18:53:45 -0800

    exercise 02 began. test 1: repeats the string passes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatNum) {
+  let repeatedString = "";
+  for (let i = 0; i < repeatNum; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
