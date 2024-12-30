a187c850cd1a7d421b9953d000196c2269725a24
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-12-30 15:05:49 -0800

    exercise 03 tests passs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = string.split("").reverse().join("");
+  return reversedString;
 };
-
+//Ill have to use a string method to extract the input string into an array of its characters. split()
+//I can save this in its own variable. arrays have the reverse() method to change the order of the array.
 // Do not edit below this line
 module.exports = reverseString;
